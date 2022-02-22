250084de6cfb689d747618781858899a8842742d
Adrier <adrierj@gmail.com>
2022-02-22 17:54:53 -0300

    java

---
+++ java/maratona_devjobo/src/maratona_devjobo/br/com/abc/javacore/Aintroducaoclasses/classes/Carro.java
@@ -0,0 +1,23 @@
+package br.com.abc.javacore.Aintroducaoclasses.classes;
+
+/**
+ * Created by William Suane on 3/10/2016.
+ */
+public class Carro {
+    public String placa;
+    public String modelo;
+    public float velocidadeMaxima;
+
+    public Carro(String bmw, double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void imprime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void imprime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
