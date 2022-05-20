02543d5c91b3b9930de273fecaa64a01e9cd7841
adrier <adrierj@gmail.com>
2022-05-19 21:50:22 -0300

    dicionario

--- Python_2022.1/Aulas - Estrutura de dados/Exercicio Dicio/exercicio_dicio.py
+++ Python_2022.1/Aulas - Estrutura de dados/Exercicio Dicio/exercicio_dicio.py
@@ -1,12 +1,12 @@
 def rotate_word(word, value):
     word_rot = ''
     for letter in word:
-        codigo = ord(letter) + value #comando ord retorna inteiro da tabela ASC
+        codigo = ord(letter) + value #comando ord retorna inteiro da tabela ASCII # soma com o valor
         if codigo > ord('z'):
             codigo -=26
         elif codigo < ord('a'):
             codigo +=26
-        word_rot += chr(codigo)
+        word_rot += chr(codigo)#converte de ASCII para char
     return word_rot
 
 print(rotate_word('adrier',2))
