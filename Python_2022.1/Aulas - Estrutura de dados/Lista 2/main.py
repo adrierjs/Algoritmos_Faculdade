9bc4cbcba3cac98589d1bf521cdcbb5e86c38a98
adrier <adrierj@gmail.com>
2022-05-31 22:57:26 -0300

    estrutura_de_dados

--- Python_2022.1/Aulas - Estrutura de dados/Lista 2/main.py
+++ Python_2022.1/Aulas - Estrutura de dados/Lista 2/main.py
@@ -31,14 +31,15 @@ class LinkedList:
         else:
             raise IndexError('Lista vazia.')
 
-    def __setitem__(self,index, value):
-        if self.head:
+    def __setitem__(self, index, value):
+        if (self.head):
+            aux = self.head
             for i in range(index):
-                if aux.next:
+                if (self.head):
                     aux = aux.next
                 else:
-                    raise IndexError('Fora da lista')
-                aux.data = value
+                    raise IndexError('Elemento fora da lista')
+            aux.data = value
         else:
             raise IndexError('Lista vazia')
 
@@ -56,30 +57,61 @@ class LinkedList:
         output += ']'
         return output
 
+    def removeUltimo(self):
+        if (self.head):
+            aux1 = self.head #aux1 recebe o inicio
+            aux2 = aux1 #copia de aux1
+            while aux1.next: #se aux1 não tiver apontando para None
+                aux2 = aux1 #faz novamente uma copia de aux1
+                aux1 = aux1.next #faz o ponteiro avançar
+            if (aux2 == None): #se aux1 == None, é pq chegou no fim
+                del aux1  #deleta aux 1
+                self.head = None
+            else:
+                aux2.next = None
+                del aux1
+
+        else:
+            raise IndexError('Lista vazia')
+
     def extend(self, lista):
         tam = lista.__len__()
         for i in range(tam):
             self.append(lista.__getitem__(i))
 
     # Questao2
-    def insert(self):
-        pass
+    def insert(self, index, value):
+        tam = self.__len__()
+        aux = LinkedList()
+        for i in range(index, tam):
+            aux.append(self.__getitem__(i))
+            self.__setitem__(index,value)
+        if index >= tam:
+            l.append(value)
+        self.extend(aux)
+        return self
 
+#começo da questao 3
     def pop(self):
+        self.removeUltimo()
 
 
+    def pop(self, index):
+        pass
 
 
 
 
 
 l = LinkedList()
-l2 = LinkedList()
-l.append(20)
-l2.append(30)
-l2.append(10)
-l2.append(13)
-l.extend(l2)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(5)
+l.insert(3,4)
+l.insert(5,6)
 print(l)
+
+
 
 
