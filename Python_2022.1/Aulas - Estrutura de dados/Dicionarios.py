13aa85a89936ab5cb15320cd6148a3398aea48dd
Adrier <adrierj@gmail.com>
2022-07-10 21:48:49 -0300

    arvores

--- Python_2022.1/Aulas - Estrutura de dados/Dicionarios.py
+++
@@ -1,8 +0,0 @@
-dicionario = {}
-
-dicionario.__setitem__("Adrier",10)
-print(dicionario.keys()) #retorna somente as chaves
-print(dicionario.values()) #valor
-print(dicionario.get("Adrier")) #retorna do valor da chave fornecida, caso não tenha nada, retorna None
-
-
